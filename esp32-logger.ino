2f6bf8a57013298a70c94d8c4020c4be7162e216
Jack Burgess <jackburgess124@gmail.com>
2020-04-08 23:46:22 +0100

    BMP280 temperature + pressure read

--- esp32-logger.ino
+++ esp32-logger.ino
@@ -5,6 +5,7 @@
 #include <WiFiUdp.h>
 #include <NTPClient.h>
 #include <HTTPClient.h>
+#include <Adafruit_BMP280.h>
 
 #include "memory.h"
 #include "network.h"
@@ -13,6 +14,7 @@
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "uk.pool.ntp.org", 0, 1000);
 Node* node;
+Adafruit_BMP280 bmp;
 
 void initNtp() {
   timeClient.begin();
@@ -30,18 +32,39 @@ void setup() {
 
   pinMode(LED_PIN, OUTPUT);
 
+  Serial.print("Flash size: ");
+  Serial.println(ESP.getFlashChipSize());
+  boolean status = bmp.begin(0x76);
+  if (!status) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+  }
   node = new Node();
   initNtp();
+
 }
+
+#define SEALEVELPRESSURE_HPA (1024.50)
 
 void loop() {
   // TODO sensors
   // node->logTemperature();
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
 
   Serial.print(timeClient.getFormattedTime());
   Serial.print("\t");
   Serial.println(node->readLight());
 
-  node->sleep();
-  node->wake();
+  /* node->sleep(); */
+  /* node->wake(); */
+  delay(5 * 1000);
 }
