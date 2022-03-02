44e02528b394e3316dba14a79464627872c6010f
Jack Burgess <jackburgess124@gmail.com>
2022-03-02 22:26:16 +0000

    significantly increase stability by preventing multiple influx connections at once with a semaphore

--- influx.cpp
+++ influx.cpp
@@ -2,11 +2,14 @@
 #include "credentials.h"
 
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB);
+SemaphoreHandle_t mutex = xSemaphoreCreateMutex();
 
 bool validateInfluxConnection() {
   client.setWriteOptions(WriteOptions().writePrecision(WritePrecision::MS));
   client.setHTTPOptions(HTTPOptions().connectionReuse(true));
+  xSemaphoreTake(mutex, portMAX_DELAY);
   bool influxOk = client.validateConnection();
+  xSemaphoreGive(mutex);
   if (influxOk) {
     Serial.print(F("[ INFLUX ] Connected to: "));
     Serial.println(client.getServerUrl());
@@ -23,7 +26,10 @@ uint8_t failedCount = 0;
 bool logPoint(Point &point) {
   Serial.print(F("[ INFLUX ] Writing: "));
   Serial.println(client.pointToLineProtocol(point));
-  if (!client.writePoint(point)) {
+  xSemaphoreTake(mutex, portMAX_DELAY);
+  bool writeOk = client.writePoint(point);
+  xSemaphoreGive(mutex);
+  if (!writeOk) {
     int16_t lastStatusCode = client.getLastStatusCode();
     if (lastStatusCode == 204) {
       // We didn't send any data!
